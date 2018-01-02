6bbbf14545f46a47180c6f5745cb9152bf802cc0
Matheus Paiva <matheus.a.paiva@gmail.com>
2018-01-02 13:57:10 -0200

    Add some reminders
    Add some reminders for installing some apps manually

--- INIT.sh
+++ INIT.sh
@@ -79,3 +79,12 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 dnf check-update
 sudo dnf install code
+
+
+# -------------------------------------------------------------------------------------------
+# Some reminders
+# -------------------------------------------------------------------------------------------
+echo "Do not forget to install Slack"
+echo "Do not forget to install Skype"
+echo "Do not forget to install Google Chrome"
+echo "Do not forget to install Spotify"
